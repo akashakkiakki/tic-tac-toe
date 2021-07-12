fc791a89cbaf1fc65a7e2e0efc6d505ffcd8aa8a
Akash sharma <akk6928@gmail.com>
2021-07-12 14:55:58 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
